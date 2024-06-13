1975c61f69028e4074375cbc2d9a1b112aef5e40
AhmadLiaqat1036 <ahmad.liaqat@unduit.pk>
2024-06-13 18:28:07 +0500

    Day 2 Changes-> Added TableView and tableViewCells in HomeVC

---
+++ AuctionMania/Home/BigTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  BigTableViewCell.swift
+//  AuctionMania
+//
+//  Created by usear on 6/13/24.
+//
+
+import UIKit
+
+class BigTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var CarTypeConstraintBetweenLogoAndText: NSLayoutConstraint!
+    @IBOutlet weak var CarImage: UIImageView!
+    
+    @IBOutlet weak var BackgroundCellView: UIView!
+    @IBOutlet weak var CarCompanyImage: UIImageView!
+    @IBOutlet weak var CarComapanyName: UILabel!
+    @IBOutlet weak var CarName: UILabel!
+    @IBOutlet weak var BidBackground: UIView!
+    @IBOutlet weak var Bid: UILabel!
+    @IBOutlet weak var CarTypeBackground: UIView!
+    @IBOutlet weak var CarType: UILabel!
+    @IBOutlet weak var TimeLeftBackground: UIView!
+    @IBOutlet weak var TimeLeft: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+
+        super.setSelected(selected, animated: animated)
+        BackgroundCellView.layer.cornerRadius = 10
+        BackgroundCellView.layer.borderWidth = 1
+        BackgroundCellView.layer.borderColor = UIColor.label.cgColor
+        BidBackground.layer.cornerRadius=20
+        CarTypeBackground.layer.cornerRadius=20
+        TimeLeftBackground.layer.cornerRadius=20
+        
+        if CarType.text?.count ?? 0 < 7{
+            CarTypeConstraintBetweenLogoAndText.constant = 12
+        }else{
+            CarTypeConstraintBetweenLogoAndText.constant = 5
+        }
+        if Bid.text?.count ?? 0 > 7{
+            Bid.text? = (Bid.text?.prefix(7) ?? "")+"+"
+        }
+        
+    }
+    
+}
--- AuctionMania/Home/HomeViewController.swift
+++ AuctionMania/Home/HomeViewController.swift
@@ -8,24 +8,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Home"
-//        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.isHidden = false
-        view.backgroundColor = .green
+        let nib = UINib(nibName: "BigTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "BigTableViewCell")
+        tableView.showsVerticalScrollIndicator = false
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 220
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BigTableViewCell", for: indexPath) as? BigTableViewCell else {return UITableViewCell()}
+        switch indexPath.row{
+        case 0:
+            cell.CarImage.image =  UIImage(named: "images-2")
+            cell.CarType.text = "Sport"
+        case 1:
+            cell.CarImage.image =  UIImage(named: "images-3")
+            cell.CarType.text = "Hypercar"
+        case 2:
+            cell.CarImage.image =  UIImage(named: "images-4")
+            cell.CarType.text = "Sedan"
+            
+        case 3:
+            cell.CarImage.image =  UIImage(named: "images")
+            cell.CarType.text = "SUV"
+        default:
+            break
+        }
+                return cell
     }
-    */
-
 }
