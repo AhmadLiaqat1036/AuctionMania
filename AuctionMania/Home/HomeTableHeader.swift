af72047944b98263946d5edb7d5379646ff830e6
AhmadLiaqat1036 <ahmad.liaqat@unduit.pk>
2024-06-14 19:58:20 +0500

    Day 3 - Added a HomeTableHeader

---
+++ AuctionMania/Home/HomeTableHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableHeader.swift
+//  AuctionMania
+//
+//  Created by usear on 6/14/24.
+//
+
+import UIKit
+
+class HomeTableHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
